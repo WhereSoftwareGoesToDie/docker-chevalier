8bae1fd6c6a5cc5f793d8b9b19cdbd10b34c6cb4
Katie McLaughlin <katie@glasnt.com>
2015-02-09 14:14:42 +1100

    Docker + exposed ports behave a little nicer than linking
    update entrypoint to suit

--- chevalier/docker-entrypoint.sh
+++ chevalier/docker-entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -e
 
-# having a container aliased as "es" and exposing port 9200 generates 
-# the environment variable ES_PORT_9200_TCP. Explode if we don't see
-# this special port opened
-
-if [ -z "$ES_PORT_9200_TCP" ]; then
-	echo >&2 "error: missing ES_PORT_9200_TCP environment variable"
-	echo >&2 "   Did you forget to --link my_elasticsearch:es ?"
-	exit 1
-fi
-
-exec "$@"
-
+chevalierd -read
