288a0c6ff1343d7dbc2e05c5b3336f3dca506270
Katie McLaughlin <katie@glasnt.com>
2015-02-09 16:48:22 +1100

    Use docker-entrypoint to inject environment variables
    Allows for chevalier image to be able to be run in multiple
    environments, by allowing run-time config options
    
    Sample invocation:
    
        docker run -e ES_HOST=localhost -e BROKER_HOST=chateau.local --name chevalier anchor/chevalier

--- chevalier/docker-entrypoint.sh
+++ chevalier/docker-entrypoint.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 set -e
 
+CONFIG_FILE=/etc/chevalier.gcfg
+
+
+# Replace KEY with VALUE in FILE
+function set_config() {
+	sed -i "s/\($1 *= *\).*/\1$2/" $3
+}
+
+if [ -z "$ES_HOST" ]; then
+	echo >&2 "error: missing ES_HOST environment variable. Defaulting to the value in $CONFIG_FILE"
+else
+	set_config "host" $ES_HOST /etc/chevalier.gcfg
+fi
+
+if [ -z "$BROKER_HOST" ]; then
+	echo >&2 "error: missing BROKER_HOST environment variable. Defaulting to the value in $CONFIG_FILE"
+else
+	BROKER_TCP="tcp:\/\/$BROKER_HOST:5580"
+	set_config "contentsendpoint" $BROKER_TCP /etc/chevalier.gcfg
+fi
+
+cat /etc/chevalier.gcfg
+
 chevalierd -read
